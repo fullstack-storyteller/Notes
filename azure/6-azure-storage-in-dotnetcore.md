7d5ab40177fe8f0c43f5a8caa48930f421e3e6e8
Manish Kumar <manishmatrix@outlook.com>
2021-07-14 02:07:39 +0530

    Learning Azure paired region

---
+++ azure/6-azure-storage-in-dotnetcore.md
@@ -0,0 +1,14 @@
+# How to use the azure storage in .net core
+
+1. Create a .net core console application.
+2. Then import/install the nuget package `Azure.Storage.blobs` for interacting with Azure storage account service.
+3. Choose your project and install, please refer below
+   ![using blob service in .netcore](./images/12.png)
+
+4. Create a Blob in your azure account, say `data`
+5. Go the corresponding storage account and get the `Access Keys` from the portal
+6. Click on Show Keys and copy the anyone of the connection strings to use it in your .net core console app
+
+7. Follow the programming as shown in `GITHUB\Notes\dotnet\AzureBlobOnDotNetCore\`
+8. Refer the image of output below:
+   ![output of blob storage from .netcore](./images/13.png)
---
+++ dotnet/AzureBlobOnDotNetCore/Program.cs
@@ -0,0 +1,53 @@
+﻿using Azure.Storage.Blobs;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AzureBlobOnDotNetCore
+{
+    class Program
+    {
+
+        //storage account name : manishapp0111
+
+        //access key:
+        static string connectionString="DefaultEndpointsProtocol=https;AccountName=manishapp0111;AccountKey=YdU3LlUbY5niJMYnbgkBL3elyRa34HXxNennbTDy44GgOno+tPLPlrb1Pe+YdVCzLhuk/+rsk+JeqluGHDR6DQ==;EndpointSuffix=core.windows.net";
+        static string fileToBeCopiedToCloudStorageBlob=@"D:\GITHUB\Notes\dotnet\1-dotnet-basics.md";
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //created client for the blob service
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+
+            //name of the blob container, in our case its 'data'
+            BlobContainerClient blobContainerClient = blobServiceClient.GetBlobContainerClient("data");
+
+            //using this name they will create the file from the local fileToBeCopiedToCloudStorageBlob
+            BlobClient blobClient = blobContainerClient.GetBlobClient("example.md");
+
+            //open the file to upload
+            using FileStream uploadFileStream = File.OpenRead(fileToBeCopiedToCloudStorageBlob);
+
+            //await while it get uploaded
+            await blobClient.UploadAsync(uploadFileStream, true);
+
+            uploadFileStream.Close();
+
+            Console.WriteLine("File Uploaded!");
+
+
+
+            Console.WriteLine("Operation Complete.");
+
+
+
+
+
+
+
+
+        }
+    }
+}
